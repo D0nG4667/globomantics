33c495bd6b295cd8bda7609db6fc53a9a7f331db
D0nG4667 <D0nG4667@users.noreply.github.com>
2023-08-05 14:18:47 +0300

    DONE- fetchHouses & featuredHouse

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,8 +1,28 @@
-import logo from './logo.svg';
 import './main-page.css';
 import Header from './header';
+import { useEffect, useMemo, useState } from 'react';
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const res = await fetch("/houses.json");
+      const houses = await res.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
+
+
   return (
     <div className="container">
       <Header subtitle="Providing houses all over the world"/>
